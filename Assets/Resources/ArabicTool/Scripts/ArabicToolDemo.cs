d5a6e6e7d8a9a6d24c1f46bd165daaedb83149d3
BISHER-PC\Bisher Dokkmak <bisher.d790@gmail.com>
2020-12-31 01:08:55 +0800

    [Setup] Setup project for shooting.

---
+++ Assets/Resources/ArabicTool/Scripts/ArabicToolDemo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ArabicTool;
+using System.Text;
+
+public class ArabicToolDemo : MonoBehaviour
+{
+    [SerializeField] private SeniorText seniorText;
+    [SerializeField] private string arabicText;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetArabText();
+    }
+
+    void SetArabText()
+    {
+        //Debug.LogError("TestArabText");
+
+        // Use SeniorText of plugin
+        if(seniorText)
+        {
+            SeniorText text1 = seniorText.GetComponent<SeniorText>();
+            text1.text = "";
+            text1.text = null;
+
+            text1.text = arabicText;
+        }
+
+        string co = ArabicTextTool.Convert("");
+        string rstr = ArabicTextTool.ReverseOnly(co);
+
+    }
+
+}
