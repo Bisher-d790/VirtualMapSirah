1575ff6a1c13a2f7de2d5afc0192e192396b2777
BISHER-PC\Bisher Dokkmak <bisher.d790@gmail.com>
2021-01-05 00:34:01 +0800

    [Setup] Setup for Sham and Tabuk. [Record] Sham recordings.

--- Assets/Scripts/AnimationController_Sham.cs
+++ Assets/Scripts/AnimationController_Sham.cs
@@ -10,6 +10,10 @@ public class AnimationController_Sham : AnimationController_Base
     [SerializeField] private ArabicConvert_Base bottomPanelText;
     [SerializeField] private ArabicConvert_Base sidePanelText;
 
+    [Header("Clip-4")]
+    [SerializeField] private float startTimescaleDelay;
+    [SerializeField] private float timescaleDelay;
+
     IEnumerator PlayClip_1()
     {
         Time.timeScale = 0.1f;
@@ -18,6 +22,56 @@ public class AnimationController_Sham : AnimationController_Base
         currentTime.arabicText = "رجب \n 9هـ";
         currentTime.SetArabText();
 
+        bottomPanelText.arabicText = "جمع\n الروم لحرب النبي صلى الله عليه وسلم";
+        bottomPanelText.SetArabText();
+
         yield return new WaitForEndOfFrame();
+    }
+    IEnumerator PlayClip_2()
+    {
+        Time.timeScale = 0.1f;
+
+        timelineSlider.value = 0f;
+        currentTime.arabicText = "رجب \n 9هـ";
+        currentTime.SetArabText();
+
+        sidePanelText.arabicText = " خوف الروم \n\n من قيام الدولة \n\n الإسلامية الحديثة ";
+        sidePanelText.SetArabText();
+
+        yield return new WaitForEndOfFrame();
+    }
+    IEnumerator PlayClip_3()
+    {
+        Time.timeScale = 0.1f;
+
+        bottomPanelText.arabicText = "هرب الجيش الروماني";
+        bottomPanelText.SetArabText();
+
+        timelineSlider.value = 0.5f;
+        currentTime.arabicText = "رجب \n 15";
+        currentTime.SetArabText();
+
+        yield return new WaitForEndOfFrame();
+    }
+    IEnumerator PlayClip_4()
+    {
+        Time.timeScale = 0.1f;
+
+        bottomPanelText.arabicText = "معجزة الآبار";
+        bottomPanelText.SetArabText();
+
+        yield return new WaitForSeconds(startTimescaleDelay);
+
+        for (int i = 1; i <= 15; i++)
+        {
+            //Debug.Log("timeline: " + (i * 10 / 30) + 
+            //    "\nround: " + ((i * 10 / 30) * 0.1f));
+
+            timelineSlider.value = ((i * 10 / 30) * 0.1f);
+            currentTime.arabicText = "رجب \n " + i.ToString();
+            currentTime.SetArabText();
+
+            yield return new WaitForSeconds(timescaleDelay);
+        }
     }
 }
